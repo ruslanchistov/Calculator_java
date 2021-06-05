0893d4cb055827a105ba0851c26aea97fa508110
ruslanchistov <r.chistov@bk.ru>
2021-06-05 21:25:54 +0300

    work continues

--- src/calculator/Panel.java
+++ src/calculator/Panel.java
@@ -1,19 +1,69 @@
 package calculator;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Panel extends JPanel {
-    final JButton[] number = new JButton[10];
+    final JButton[] button = new JButton[20];
+    final String[] liter = {"C", "<", "SQ", "%", "1", "2", "3", "--", "4", "5",
+            "6", "+", "7", "8", "9", "/", "0", ".", "=", "*"};
+    final Font font = new Font("SanSerif", Font.BOLD, 20);
+    final JTextField screen = new JTextField();
 
     public Panel() {
         setLayout(null);
-
-        for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
-                number[x * 3 + y + 1] = new JButton("" + (x * 3 + y + 1));
-                number[x * 3 + y + 1].setBounds(y * 80 + 10, x * 80 + 90, 70, 70);
-                add(number[x * 3 + y + 1]);
+        screen.setBounds(10, 10, 310, 70);
+        screen.setEditable(false);
+        screen.setFont(font);
+        add(screen);
+        int i = 0;
+        for (int y = 90; y <= 410; y += 80) {
+            for (int x = 10; x <= 250; x += 80) {
+                button[i] = new JButton(liter[i]);
+                button[i].setBounds(x, y, 70, 70);
+                button[i].setFont(font);
+                add(button[i]);
+                i++;
             }
+        }
+        ActionListener num = (ActionEvent e) -> {
+            JButton b = (JButton) e.getSource();
+            screen.setText(screen.getText() + b.getText());
+        };
+        int[] pos_num = {4, 5, 6, 8, 9, 10, 12, 13, 14, 16};
+        for (int p : pos_num) {
+            button[p].addActionListener(num);
+        }
+        ActionListener serv = (ActionEvent e) -> {
+//            нажатие клавиши "сброс"
+            if (e.getSource() == button[0]) {
+                screen.setText(null);
+            }
+//            нажатие клавиши "стирание справа"
+            if (e.getSource() == button[1]) {
+                String str = screen.getText();
+                if (str.length() > 0) {
+                    screen.setText(str.substring(0, str.length() - 1));
+                }
+            }
+//            нажатие клавиши "десятичная точка"
+            if(e.getSource()==button[17]){
+                String str = screen.getText();
+                if (str.length()==0){
+                    screen.setText("0.");
+                }
+                else if(! str.contains(".")){
+                    screen.setText(str+".");
+                }
+            }
+        };
+        int[] pos_serv = {0, 1, 2, 3, 7, 11, 15, 17, 18, 19};
+        for (int p : pos_serv) {
+            button[p].addActionListener(serv);
         }
     }
 }
+
+
--- src/calculator/Window.java
+++ src/calculator/Window.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Window {
     public static void main(String[] args) {
         JFrame window = new JFrame("Калькулятор");
-        window.setSize(400, 500);
+        window.setSize(345, 530);
         window.setLocationRelativeTo(null);
         window.setResizable(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
